9c5dc07b05f979b5a2554b0a9b6fcaa3b91cb6dc
Zzyzx <rustyshackleford3@protonmail.com>
2019-09-18 14:41:45 -0700

    grid is now resizable

--- script.js
+++ script.js
@@ -1,35 +1,30 @@
-function draw() {
-    let cell = document.getElementsByClassName('cell');
-    for (let i = 0; i < cell.length; i++) {
-        cell[i].addEventListener('mouseover', () => {
-            cell[i].setAttribute('style', 'background: black');
+function genDivs(numCells) {
+    for (let i = 0; i < numCells*numCells; i++) {
+        let cell = document.createElement('div');
+        cell.className = 'cell';
+        document.documentElement.style.setProperty("--numCol", numCells);
+        container.appendChild(cell);
+        cell.addEventListener("mouseover", function(e){
+            e.target.style.background = "black";
         });
     }
 }
 
-function genDivs(numCells) {
-    for (let i = 0; i < numCells; i++) {
-        let row = document.createElement('div');
-        row.className = 'row';
-        container.appendChild(row);
-        for (let j = 0; j < numCells; j++) {
-            let cell = document.createElement('div');
-            cell.className = 'cell';
-            row.appendChild(cell);
+function reset() {
+    let input = prompt('How many squares per side? (Max 64)');
+    let size = parseInt(input);
+    if (Number.isInteger(size) && size < 65 && size > 0) {
+        while (container.hasChildNodes()) {
+            container.removeChild(container.lastChild);
         }
+    genDivs(size);
+    } else {
+        alert("Please enter a number between 1 - 64");
+        reset();
     }
 }
 
-function removeDivs() {
-    
-}
-
-function reset() {
-    let size = prompt('How many squares per side?');
-}
-
 window.onload = genDivs(16);
-window.addEventListener('load', draw, true);
 
 let resetBtn = document.querySelector('button');
 resetBtn.onclick = () => reset();
